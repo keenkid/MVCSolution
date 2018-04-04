59ffa041b60c1ad980e118f7b95c13acd673321a
keenkid.mao <keenkid.mao@fisglobal.com>
2018-04-04 22:14:48 +0800

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            var declaration = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", string.Empty);
+            xmlDoc.AppendChild(declaration);
+                        
+            var ndRoot = xmlDoc.CreateNode(XmlNodeType.Element, "root", string.Empty);
+            var ndList = xmlDoc.CreateNode(XmlNodeType.Element, "list", string.Empty);
+            ndRoot.AppendChild(ndList);
+
+            var ndRefID = xmlDoc.CreateNode(XmlNodeType.Element, "refID", string.Empty);
+            ndRefID.InnerText = "343434";
+            ndList.AppendChild(ndRefID);
+
+            xmlDoc.AppendChild(ndRoot);
+            
+            xmlDoc.Save(@"c:\users\keenkid.mao\desktop\test.xml");
+            Console.WriteLine(xmlDoc.OuterXml);
+        }
+    }
+}
---
+++ MVCFirstApp/Controllers/NewHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFirstApp.Controllers
+{
+    public class NewHomeController : Controller
+    {
+        // GET: NewHome
+        public string AnotherChance()
+        {
+            return "Keenkid exception test";
+        }
+    }
+}
